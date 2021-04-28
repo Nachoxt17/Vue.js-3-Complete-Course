b027e8c4b3cb142bbf3b905b64d24409cfdeca37
Ignacio Ceaglio <ignacioceaglio@gmail.com>
2021-04-28 19:03:29 -0300

    Added Eslint

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<!--+-Component HTML Part:_-->
+<template>
+  <button class="btn">{{ text }}</button>
+</template>
+
+<!--+-Component Logic JS Part:_-->
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: String,
+    color: String,
+  }
+}
+</script>
+
+<!--+-Component CSS Styles Part:_(Scoped means that THIS styles Only apply for THIS Component)-->
+<style scoped>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<!--+-Component HTML Part:_-->
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green" />
+  </header>
+</template>
+
+<!--+-Component Logic JS Part:_-->
+<script>
+import Button from './Button';
+
+export default {
+  name: 'Header',
+  props: {
+    title: {
+      type: String,
+      default: 'Hello world'
+    },
+  },
+  components: {
+    Button,
+  }
+}
+</script>
+
+<!--+-Component CSS Styles Part:_(Scoped means that THIS styles Only apply for THIS Component)-->
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+ }
+</style>
